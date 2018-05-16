cef3253a27a719b8d2d8bbb52dfe661b627f15d6
lognxn <longxiaonan@163.com>
2018-05-16 20:53:35 +0800

    调整core包结构

--- vdp-cloud-vlr/src/main/java/com/vdp/vlr/VLRApplication.java
+++ vdp-cloud-vlr/src/main/java/com/vdp/vlr/VLRApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages= {"com.vdp.core", "com.vdp"})
+@ComponentScan(basePackages= {"com.vdp.core", "com.vdp.vlr"})
 public class VLRApplication {
 
     public static void main(String[] args) {
--- vdp-cloud-vlr/src/main/java/com/vdp/vlr/rabbit/controller/MqController.java
+++ vdp-cloud-vlr/src/main/java/com/vdp/vlr/rabbit/controller/MqController.java
@@ -1,28 +1,12 @@
 package com.vdp.vlr.rabbit.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.vdp.vlr.rabbit.sender.HelloSender;
+import com.vdp.core.tools.redis.config.RedisProps;
 
 @RestController
 public class MqController {
 	@Autowired
-    private HelloSender helloSender;
-	
-	@GetMapping("/sender1")
-	public void test1(){
-		helloSender.sendDirectMsg();
-	}
-	
-	@GetMapping("/sender2")
-	public void test2(){
-		helloSender.sendTopicMsg();
-	}
-	
-	@GetMapping("/sender3")
-	public void test3(){
-		helloSender.sendFanoutMsg();
-	}
+	private RedisProps props;
 }
